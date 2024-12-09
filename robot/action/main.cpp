f303023f978a266e639d9fe1c6ef6e81be84de50
stroth-hub <st.roth@ostfalia.de>
2024-12-09 12:31:02 +0100

    Update main.cpp

--- robot/action/main.cpp
+++ robot/action/main.cpp
@@ -444,7 +444,7 @@ int main()
 		else if(strcmp(buffer,"no_cmd") == 0)
 		{
 			//printf("No Command");
-			if(CM730::Ping(200,int_err)!=0)
+			if(cm730.CM730::Ping(200,int_err)!=0)
 			{
 				//printf(*int_err);
 				cm730.Connect();
