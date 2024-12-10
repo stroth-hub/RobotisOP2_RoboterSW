f387e3e5e076dfcd54c5c48d7ec59c5acfa09866
stroth-hub <st.roth@ostfalia.de>
2024-12-10 12:15:47 +0100

    Update main.cpp

--- robot/action/main.cpp
+++ robot/action/main.cpp
@@ -447,7 +447,7 @@ int main()
 			{
 				printf("cm730 Disconnected \n");
 				cm730.Connect();
-				MotionManager::GetInstance()->Reinitialize()
+				MotionManager::GetInstance()->Reinitialize();
 				usleep(8*1000);
 				//Scan(&cm730);
 				Action::GetInstance()->Start(1);
